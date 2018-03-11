ff3b734d3933d2014ff85a6d1172950ce631517c
NavitaK <navitakdev@gmail.com>
2018-03-11 20:19:48 +0300

    Moved field name to static final field

--- src/test/java/gson/objects/Address.java
+++ src/test/java/gson/objects/Address.java
@@ -4,13 +4,15 @@ import com.google.gson.JsonElement;
 
 public class Address extends DomainObject {
 
+    public static final String STREET_FIELD = "street";
+
     public String getStreet() {
-        JsonElement streetElement = jsonObject.get("street");
+        JsonElement streetElement = jsonObject.get(STREET_FIELD);
         return streetElement.isJsonNull() ? null: streetElement.getAsString();
     }
 
     public void setStreet(String street) {
-        jsonObject.addProperty("street", street);
+        jsonObject.addProperty(STREET_FIELD, street);
     }
 
     // other getters and setters for Address fields
