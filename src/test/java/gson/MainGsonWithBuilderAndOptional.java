e2b56dfe860053a3562869bcbb6779e6b33902ec
NavitaK <navitakdev@gmail.com>
2018-03-11 23:21:09 +0300

    Added check that address has street

--- src/test/java/gson/MainGsonWithBuilderAndOptional.java
+++ src/test/java/gson/MainGsonWithBuilderAndOptional.java
@@ -12,15 +12,23 @@ public class MainGsonWithBuilderAndOptional {
         address.setCustomStringField("unknownProperty", "value");
         address.setStreet("Kolasa");
 
-        System.out.println(address.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
-        System.out.println(address.getCustomStringField("unknownProperty").orElseThrow(
+        if (address.hasStreet()) {
+            System.out.println(address.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
+        }
+        if (address.hasCustomStringField("unknownProperty")) {
+            System.out.println(address.getCustomStringField("unknownProperty").orElseThrow(
                 () -> new TestNGException("CustomField not set")));
+        }
 
         AddressWithOptional address2 = new AddressBuilderWithOptional().withRequiredFieldsOnly().build();
-        System.out.println(address2.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
+        if (address.hasStreet()) {
+            System.out.println(address2.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
+        }
 
         AddressWithOptional address3 = new AddressBuilderWithOptional().withAllFields().build();
-        System.out.println(address3.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
+        if (address.hasStreet()) {
+            System.out.println(address3.getStreet().orElseThrow(() -> new TestNGException("Street not set")));
+        }
     }
 
 }
